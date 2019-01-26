e596f5d89c1e8eb9563a5ef879957f50b706dcd1
aimanafzal <ayman.afzal@msn.com>
2019-01-26 15:22:58 +0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import {render} from 'react-dom';
 import createHashHistory from 'history/lib/createHashHistory';
 import {Router, useRouterHistory} from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import routes from './routes';
+import routes from './server/routes';
 import './index.css';
+
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
