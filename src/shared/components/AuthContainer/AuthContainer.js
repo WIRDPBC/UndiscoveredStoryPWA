fd2ac98112c9f8948b73a1138d8bdcce1d9691e4
aimanafzal <ayman.afzal@msn.com>
2019-02-27 00:59:12 +0500

    Add files via upload

--- src/shared/components/AuthContainer/AuthContainer.js
+++ src/shared/components/AuthContainer/AuthContainer.js
@@ -3,15 +3,20 @@
  * 21st Feb 2019
  */
 
-import React, {PureComponent} from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types';
-import {Grid} from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 
 //import css
 import './AuthContainer.css'
 
 
-class AuthContainer extends PureComponent{
+
+// Importing Images
+
+import Login from '../../icons/5_Login.jpg';
+
+class AuthContainer extends PureComponent {
     static propTypes = {
 
     }
@@ -20,19 +25,21 @@ class AuthContainer extends PureComponent{
 
     }
 
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
-    render(){
+    render() {
         return (
-            <Grid columns={2} style={{padding: 0}} className="auth-container">
-                <Grid.Column width={10} style={{padding: 0}}>
+            <Grid columns={2} style={{ padding: 0 }} className="auth-container">
+                <Grid.Column width={10} style={{ padding: 0 }}>
                     <div className="auth-container-image-container">
-                        <div className="auth-container-image-placeholder"></div>
+                        <div className="auth-container-image-placeholder">
+                            <img src={Login} style={{ height: "500px", width: "500px" }} />
+                        </div>
                     </div>
                 </Grid.Column>
-                <Grid.Column width={6} style={{padding: 0}}>
+                <Grid.Column width={6} style={{ padding: 0 }}>
                     <div className="auth-container-form-container">
                         {this.props.children}
                     </div>
