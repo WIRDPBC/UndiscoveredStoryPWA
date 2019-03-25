0aecf1cbc90fca53a22011061e19e96eae2d1744
AIMAN AFZAL <ayman.afzal@msn.com>
2019-03-25 17:51:40 +0500

    auth tokens can be destroyed with email or authtoken

--- src/server/Modal/utilities.js
+++ src/server/Modal/utilities.js
@@ -2,10 +2,24 @@ const config = require('.././config').config;
 const _firebase = require('firebase');
 if (!_firebase.apps.length)
     _firebase.initializeApp(config)
+/**
+ * A general object to contain all the useful methods to be used frequently by the developer
+ * and few of them can be used as an API
+ * ex:
+ * destroyAuthenticationTokenByAuthenticationToken
+ * getTotalNumberOfRegisteredUsers
+ * destroyAuthenticationTokenByEmail
+ */
 function utilities() {
 
 }
 
+
+/**
+ * Returns the document ID based on the email provided
+ * @returns {documentID:data} which contains the documentID of the user
+ * @requires email
+ */
 utilities.prototype.getDocumentIDbyEmail = function (email) {
     let db = _firebase.firestore();
     let documentID = db.collection('users').where("email", "==", email).get().then(function (querySnapshot) {
@@ -22,8 +36,35 @@ utilities.prototype.getDocumentIDbyEmail = function (email) {
     return documentID;
 }
 
+/**
+ * Will return documentID based on authenticationToken provided.
+ * If the Authentication Token exists in any document
+ * it will return the documentID
+ * @argument authenticationToken
+ * @returns documentID
+ */
+utilities.prototype.getDocumentIDbyAuthenticationToken = function (authenticationToken) {
+    let db = _firebase.firestore();
+    let documentID = db.collection('users').where("authenticationToken", "==", authenticationToken).get().then(function (querySnapshot) {
+        var id;
+        querySnapshot.forEach(function (doc) {
+            id = doc.id;
+        });
+        return id;
+    }).then(function (data) {
+        return { documentID: data };
+    }).catch(function (error) {
+        console.log(error);
+    });
+    return documentID;
+}
 
 
+/**
+ * Returns email and password of user based on the document ID
+ * @returns {documentID:data} which contains the email and password based on the documentID of the user
+ * @requires docID
+ */
 utilities.prototype.getEmailAndPasswordByDocumentID = function (docID) {
     let db = _firebase.firestore();
     let documentID = db.collection('users').doc(docID).get().then(function (querySnapshot) {
@@ -40,6 +81,10 @@ utilities.prototype.getEmailAndPasswordByDocumentID = function (docID) {
     return documentID;
 }
 
+/**
+ * Returns an array with all information of all registered users
+ * @returns userdata
+ */
 utilities.prototype.getAllRegisteredUsers = function () {
     let db = _firebase.firestore();
     let documentID = db.collection('users').get().then(function (querySnapshot) {
@@ -56,18 +101,61 @@ utilities.prototype.getAllRegisteredUsers = function () {
     return documentID;
 }
 
-utilities.prototype.destroyAuthenticationTokenByEmail = function (email) {
+
+/**
+ * Destroys the Authentication Token based on the email provided
+ * @requires response object
+ * @requires email
+ * @returns {Message:`authentication token against ${email} has been destroyed`}
+ */
+utilities.prototype.destroyAuthenticationTokenByEmail = function (res, email) {
     this.getDocumentIDbyEmail(email).then((resolved) => {
         let dt = _firebase.firestore();
-        dt.collection('users').doc(resolved.documentID).update({ AuthenticationToken: '' })
+        dt.collection('users').doc(resolved.documentID).update({ authenticationToken: '' })
             .then(() => {
-                console.log(`authentication token against ${email} has been destroyed`);
+                res.send({ Message: `authentication token against ${email} has been destroyed` });
             })
     })
 }
 
 
-
-
+/**
+ * Returns the total number of registered users in 'users' document
+ * @requires response object to return the result directly from the method
+ * @returns {TotalRegisteredUsers: 1}
+ */
+utilities.prototype.getTotalNumberOfRegisteredUsers = function (res) {
+    let dt = _firebase.firestore();
+    dt.collection('users').get().then((querySnapshot) => {
+        let totalRegisteredUsers = 0;
+        querySnapshot.forEach(() => {
+            totalRegisteredUsers = totalRegisteredUsers + 1;
+        })
+        res.send({
+            TotalRegisteredUsers: totalRegisteredUsers
+        })
+    })
+}
+/**
+ * Destroys an Authentication token based on the
+ * authentication token provided
+ * @requires authenticationToken
+ * @requires response
+ * @returns Message
+ */
+utilities.prototype.destroyAuthenticationTokenByAuthenticationToken =
+    function (authenticationToken, res) {
+        this.getDocumentIDbyAuthenticationToken
+            .then((resolved) => {
+                let dt = _firebase.firestore();
+                dt
+                    .collection('users')
+                    .doc(resolved.documentID)
+                    .update({ authenticationToken: '' })
+                    .then(() => {
+                        res.send({Message: `authentication token against ${authenticationToken} has been destroyed`})
+                    })
+            })
+    }
 
 module.exports = utilities;
---
+++ src/server/Routes/UserRoute.js
@@ -0,0 +1,159 @@
+const express = require('express');
+const UserRouter = express.Router();
+let CreateUser = require('../Modal/UserModal');
+let UserAuthentication = require('../Modal/UserAuthentication');
+let updateUserInformation = require('../Modal/updateUserInformation');
+let questions = require('../Modal/questions');
+let utilities = require('../Modal/utilities');
+
+/**
+ *This file is used for routing to the required method
+ */
+
+UserRouter
+    .route('/create')
+    .post(function (req, res) {
+        let email = req.body.Email;
+        let password = req.body.Password;
+        if (CreateUser) {
+            let obj = new CreateUser(email, password);
+            obj.signup(res);
+        }
+
+    });
+
+
+UserRouter
+    .route('/login')
+    .post(function (req, res) {
+        let email = req.body.Email;
+        let password = req.body.Password;
+
+        if (CreateUser) {
+            let obj = new CreateUser(email, password);
+            obj.login(res);
+        }
+    });
+
+
+UserRouter
+    .route('/authenticate')
+    .post(function (req, res) {
+        let email = req.body.Email;
+        // let authenticationToken = req.body.AuthenticationToken;
+        let authenticate = req.body.Authenticate;
+        UserAuthentication = new UserAuthentication(email, authenticate);
+        UserAuthentication.getAuthenticationToken();
+        setTimeout(function () { }, 3000);
+        res.send({ token: UserAuthentication.getAuthToken() });
+    });
+
+/**
+    * Usage:
+    * {
+    * firstName: '',
+    * lastName: '',
+    * email: '',
+    * paypalEmail:'',
+    * authenticationToken: ''
+    * }
+    */
+UserRouter.route('/updateUserData').post(function (req, res) {
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    let email = req.body.email;
+    let paypalEmail = req.body.paypalEmail;
+    let obj = new updateUserInformation(firstName, lastName, email, paypalEmail);
+    obj.update(res);
+});
+
+/**
+ * Usage:
+ * {
+ * firstName:
+ * lastName:
+ * email:
+ * paypalEmail:
+ * }
+ *
+ * Response:
+ * {message: 'Information Updated'}
+ */
+UserRouter.route('/updateUserData').post(function (req, res) {
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    let email = req.body.email;
+    let paypalEmail = req.body.paypalEmail;
+    let obj = new updateUserInformation(firstName, lastName, email, paypalEmail);
+    obj.update(res);
+});
+
+/**
+ * Usage:
+ *  {
+ *  }
+ *
+ * Response:
+ *  {
+        description: "",
+        options: {
+            option1: "",
+            option2: "",
+            option3: "",
+            option4: ""
+        },
+        correctAnswer: 2,
+        explanation: ""
+    }
+ */
+UserRouter.route('/viewQuestions').post(function (req, res) {
+    let obj = new questions();
+    obj.loadPredefinedQuestions(res);
+});
+
+/**
+ * Usage:
+ * {
+ * }
+ * Response:
+ * {TotalRegisteredUsers: totalRegisteredUsers}
+ */
+UserRouter.route('/getTotalNumberofRegisteredUsers').post(function (req, res) {
+    let obj = new utilities();
+    obj.getTotalNumberOfRegisteredUsers(res);
+});
+
+
+
+/**
+ * Usage:
+ * {
+ *  authenticationToken: ""
+ * }
+ * Response:
+ * {Message: `authentication token against ${authenticationToken} has been destroyed`}
+ */
+UserRouter.route('/destroyAuthenticationTokenByAuthenticationToken').post(function (req, res) {
+    let authenticationToken = req.body.authenticationToken;
+    let obj = new utilities();
+    obj.destroyAuthenticationTokenByAuthenticationToken(authenticationToken,res);
+});
+
+
+
+
+/**
+ * Usage:
+ * {
+ *  authenticationToken: ""
+ * }
+ * Response:
+ * {Message: `authentication token against ${email} has been destroyed`}
+ */
+UserRouter.route('/destroyAuthenticationTokenByEmail').post(function (req, res) {
+    let email = req.body.email;
+    let obj = new utilities();
+    obj.destroyAuthenticationTokenByEmail(res,email);
+});
+
+module.exports = UserRouter;
