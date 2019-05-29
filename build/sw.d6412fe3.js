c5e219e8f86e939b1ad2cef3ed9bb1f29d744834
Nikita <nikitamittal2423@gmail.com>
2019-05-29 22:31:34 +0530

    Introduction Text Changed

--- build/sw.d6412fe3.js
+++
@@ -1,4 +0,0 @@
-webpackJsonp([2],{470:/*!***********************************!*\
-  !*** ./src/server/sw-register.js ***!
-  \***********************************/
-function(e,o){"serviceWorker"in navigator&&navigator.serviceWorker.register("./sw.js").then(function(e){console.log("Registration succeeded. Scope is "+e.scope)}).catch(function(e){console.log("Registration failed with "+e)})}},[470]);
